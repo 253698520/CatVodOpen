911744a9e964e5a245b4cbcbd57bdb77087eedeb
enoungh <135185602+253698520@users.noreply.github.com>
2024-04-24 21:51:59 +0800

    Update router.js

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -1,12 +1,67 @@
-import kunyu77 from './spider/video/kunyu77.js';
-import kkys from './spider/video/kkys.js';
-import push from './spider/video/push.js';
 import alist from './spider/pan/alist.js';
+
+import ts230 from './spider/book/230ts.js';
+import baozi from './spider/book/baozi.js';
 import _13bqg from './spider/book/13bqg.js';
+import bookan from './spider/book/bookan.js';
+import bengou from './spider/book/bengou.js';
+import laobaigs from './spider/book/laobaigs.js';
+import coco from './spider/book/coco.js';
+import hm from './spider/book/韩漫基地.js';
+import mhdq from './spider/book/mhdq.js';
+import tewx from './spider/book/tewx.js';
 import copymanga from './spider/book/copymanga.js';
-import ffm3u8 from './spider/video/ffm3u8.js';
+
 import douban from './spider/video/douban.js';
+import baipiaoys from './spider/video/baipiaoys.js';
 import wogg from './spider/video/wogg.js';
+import wobg from './spider/video/wobg.js';
+import bqr from './spider/video/bqr.js';
+import ffm3u8 from './spider/video/ffm3u8.js';
+import mayiya from './spider/video/mayiya.js';
+import czzy from './spider/video/czzy.js';
+import ddys from './spider/video/ddys.js';
+import tudou from './spider/video/tudou.js';
+import kkys from './spider/video/kkys.js';
+import ikanbot from './spider/video/ikanbot.js';
+import kunyu77 from './spider/video/kunyu77.js';
+import m3u8cj from './spider/video/m3u8cj.js';
+import sharenice from './spider/video/sharenice.js';
+import ttkx from './spider/video/ttkx.js';
+import meijumi from './spider/video/meijumi.js';
+import yingso from './spider/video/yingso.js';
+import nongmin from './spider/video/nongmin.js';
+import xinshijue from './spider/video/xinshijue.js';
+import anfun from './spider/video/anfun.js';
+import aiyingshi from './spider/video/aiyingshi.js';
+import boo from './spider/video/boo.js';
+import xzys from './spider/video/xzys.js';
+import yiqikan from './spider/video/yiqikan.js';
+import vcm3u8 from './spider/video/vcm3u8.js';
+import maiyoux from './spider/video/maiyoux_node.js';
+import saohuo from './spider/video/saohuo.js';
+import netflav from './spider/video/netflav.js';
+import appys from './spider/video/appys.js';
+import ng from './spider/video/ng.js';
+import pk_push from './spider/video/push.js';
+import appys from './spider/video/appys.js';
+import libvio from './spider/video/libvio.js';
+import cntv from './spider/video/cntv.js';
+import live from './spider/video/live.js';
+import bili from './spider/video/bili.js';
+import bilibili from './spider/video/bilibili.js';
+import huya from './spider/video/huya.js';
+import douyu from './spider/video/douyu.js';
+import _360ba from './spider/video/_360ba.js';
+import ktv from './spider/video/ktv.js';
+
+import avlive from './spider/video/avlive.js';
+import avm3u8 from './spider/video/avm3u8.js';
+import doll from './spider/video/doll.js';
+import netflav from './spider/video/netflav.js';
+import maiyoux from './spider/video/maiyoux_node.js';
+
+
 
 const spiders = [kunyu77, kkys, ffm3u8, douban, wogg,push, alist, _13bqg, copymanga];
 const spiderPrefix = '/spider';
