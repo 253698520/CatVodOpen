b1bc4b8642b6b58bb828e27d9be2551056baf6f0
enoungh <135185602+253698520@users.noreply.github.com>
2024-04-25 11:22:17 +0800

    Update router.js

--- nodejs/src/router.js
+++ nodejs/src/router.js
@@ -16,45 +16,55 @@ import douban from './spider/video/douban.js';
 import baipiaoys from './spider/video/baipiaoys.js';
 import wogg from './spider/video/wogg.js';
 import wobg from './spider/video/wobg.js';
+import ysche from './spider/video/ysche.js';
+import xzys from './spider/video/xzys.js';
 import bqr from './spider/video/bqr.js';
+import upyun from './spider/video/upyun.js';
+import yiso from './spider/video/yiso.js';
+import yingso from './spider/video/yingso.js';
+import pansearch from './spider/video/pansearch.js';
+import yunpanres from './spider/video/yunpanres.js';
+import xxpan from './spider/video/xxpan.js';
 import ffm3u8 from './spider/video/ffm3u8.js';
 import mayiya from './spider/video/mayiya.js';
 import czzy from './spider/video/czzy.js';
 import ddys from './spider/video/ddys.js';
+import ttian from './spider/video/ttian.js';
+import rrys from './spider/video/rrys.js';
+import subaibai from './spider/video/subaibai.js';
+import xinshijue from './spider/video/xinshijue.js';
 import tudou from './spider/video/tudou.js';
 import kkys from './spider/video/kkys.js';
+import nongmin from './spider/video/nongmin.js';
+import saohuo from './spider/video/saohuo.js';
+import ttkx from './spider/video/ttkx.js';
+import scys from './spider/video/scys.js';
 import ikanbot from './spider/video/ikanbot.js';
 import kunyu77 from './spider/video/kunyu77.js';
+import nangua from './spider/video/ng.js';
 import m3u8cj from './spider/video/m3u8cj.js';
-import sharenice from './spider/video/sharenice.js';
-import ttkx from './spider/video/ttkx.js';
 import meijumi from './spider/video/meijumi.js';
-import yingso from './spider/video/yingso.js';
-import nongmin from './spider/video/nongmin.js';
-import xinshijue from './spider/video/xinshijue.js';
 import anfun from './spider/video/anfun.js';
 import aiyingshi from './spider/video/aiyingshi.js';
 import boo from './spider/video/boo.js';
-import xzys from './spider/video/xzys.js';
 import yiqikan from './spider/video/yiqikan.js';
 import vcm3u8 from './spider/video/vcm3u8.js';
-import maiyoux from './spider/video/maiyoux_node.js';
-import saohuo from './spider/video/saohuo.js';
-import netflav from './spider/video/netflav.js';
-import appys from './spider/video/appys.js';
-import ng from './spider/video/ng.js';
-import pk_push from './spider/video/push.js';
 import appys from './spider/video/appys.js';
 import libvio from './spider/video/libvio.js';
 import cntv from './spider/video/cntv.js';
+import xiaoya from './spider/video/xiaoya.js';
+import xiaoya-alist from './spider/video/xiaoya-alist.js';
+import xiaoya-tv from './spider/video/xiaoya-tv.js';
+import sharenice from './spider/video/sharenice.js';
 import live from './spider/video/live.js';
+import youtube from './spider/video/youtube.js';
 import bili from './spider/video/bili.js';
 import bilibili from './spider/video/bilibili.js';
 import huya from './spider/video/huya.js';
 import douyu from './spider/video/douyu.js';
 import _360ba from './spider/video/_360ba.js';
 import ktv from './spider/video/ktv.js';
-
+import push from './spider/video/push.js';
 import avlive from './spider/video/avlive.js';
 import avm3u8 from './spider/video/avm3u8.js';
 import doll from './spider/video/doll.js';
@@ -63,7 +73,8 @@ import maiyoux from './spider/video/maiyoux_node.js';
 
 
 
-const spiders = [kunyu77, kkys, ffm3u8, douban, wogg,push, alist, _13bqg, copymanga];
+const spiders = [douban,baipiaoys,wogg,wobg,ysche,xzys,bqr,upyun,yiso,yingso,pansearch,yunpanres,xxpan,ffm3u8,mayiya,czzy,ddys,ttian,rrys,subaibai,xinshijue,tudou,kkys,nongmin,saohuo,ttkx,scys,ikanbot,kunyu77,nangua,m3u8cj,meijumi,anfun,aiyingshi,boo,
+                yiqikan,vcm3u8,appys,libvio,cntv,xiaoya,xiaoya-alist,xiaoya-tv,sharenice,live,youtube,bili,bilibili,huya,douyu,_360ba,ktv,push,avlive,avm3u8,doll,netflav,maiyoux,ts230,baozi,_13bqg,bookan,bengou,laobaigs,coco,hm,mhdq,tewx,copymanga,alist];
 const spiderPrefix = '/spider';
 
 /**
